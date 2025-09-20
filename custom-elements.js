226b818838906edaab941b781db5e356cad4c67d
maciejg-git <m.gralek@gmail.com>
2025-09-20 02:41:15 +0200

    update custom-elements.js

--- custom-elements.js
+++ custom-elements.js
@@ -3,10 +3,12 @@ document.addEventListener("alpine:init", () => {
     {
       tag: 'component-a',
       source: '/examples/components/component-a.html',
+      trigger: 'load',
     },
     {
       tag: 'component-b',
       source: '/examples/components/component-b.html',
+      trigger: 'load',
     },
   ]
   Alpine.$rc.makeCustomElementComponents(components)
