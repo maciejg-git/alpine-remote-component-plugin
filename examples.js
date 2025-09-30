297e6004dcce11d001a4efa4d5a297acb4dbd283
maciejg-git <m.gralek@gmail.com>
2025-10-01 00:32:17 +0200

    update examples

---
+++ examples.js
@@ -0,0 +1,33 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("example", () => {
+    return {
+      html: '',
+      handleComponentInserted() {
+        if (this.$event.detail.name === 'example') {
+          this.html = this.$event.detail.responseHTML
+
+          this.$nextTick(() => {
+            let code = this.$el.querySelectorAll('.language-javascript, .language-html')
+            if (code.length) {
+              code.forEach((el) => {
+                hljs.highlightElement(el)
+              })
+            }
+          })
+        }
+      },
+      init() {
+        let md = this.$el.querySelectorAll('.markdown')
+
+        md.forEach((el) => {
+          el.innerHTML = marked.parseInline(el.textContent)
+        })
+        this.examples[this.$el.id] = {
+          title: this.$el.querySelector('h2').textContent,
+          id: this.$el.id,
+          isVisible: false,
+        }
+      }
+    }
+  })
+})
