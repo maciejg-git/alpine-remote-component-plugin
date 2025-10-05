138ca81b5db3c5fc52b8d83ef9b81211bd0950ec
maciejg-git <m.gralek@gmail.com>
2025-10-05 18:32:32 +0200

    update tests

--- test/script.test.js
+++ test/script.test.js
@@ -16,3 +16,18 @@ it("loads component script and makes data with Alpine.data", async () => {
   expect(await screen.findByText("component-a")).toBeInTheDocument();
   expect(await screen.findByText("component content")).toBeInTheDocument();
 });
+
+it("sets _rcError if script import fails", async () => {
+  document.body.innerHTML = `
+    <div
+      x-remote-component="/component-script.html"
+      data-rc-script="/component-script-not-found.js"
+    >
+      <span x-text="_rcError"></span>
+    </div>
+  `;
+
+  Alpine.initTree(document.body);
+
+  expect(await screen.findByText("Error: Cannot find module '/component-script-not-found.js'", {exact: false})).toBeInTheDocument();
+});
