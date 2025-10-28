ebda5008bbe8186f2e049834f192c5f1c6d964eb
maciejg-git <m.gralek@gmail.com>
2025-10-29 00:32:37 +0100

    add nested component registration

--- index.js
+++ index.js
@@ -162,6 +162,9 @@ export default function (Alpine) {
         globalConfig.componentPrefix + c.tag,
         Component
       );
+      if (c.components) {
+        makeCustomElementComponents(c.components)
+      }
     });
   };
 
