baa4c7af8b3de9b0ac2a105cac42ac36bcfd7e5c
maciejg-git <m.gralek@gmail.com>
2025-11-01 15:45:34 +0100

    update build script

--- scripts/build.js
+++ scripts/build.js
@@ -3,24 +3,48 @@ let esbuild = require("esbuild")
 
 console.log(`building plugin`)
 esbuild.buildSync({
-  entryPoints: [`builds/cdn.js`],
+  entryPoints: [`remote-component/builds/cdn.js`],
   bundle: true,
   platform: "browser",
-  outfile: `dist/cdn.js`
+  outfile: `remote-component/dist/cdn.js`
 })
 console.log(`building plugin`)
 esbuild.buildSync({
-  entryPoints: [`builds/cdn.js`],
+  entryPoints: [`remote-component/builds/cdn.js`],
   bundle: true,
   platform: "browser",
   minify: true,
-  outfile: `dist/cdn.min.js`
+  outfile: `remote-component/dist/cdn.min.js`
 })
 console.log(`building plugin`)
 esbuild.buildSync({
-  entryPoints: [`builds/module.js`],
+  entryPoints: [`remote-component/builds/module.js`],
   bundle: true,
   mainFields: ["module", "main"],
   platform: "neutral",
-  outfile: `dist/module.esm.js`
+  outfile: `remote-component/dist/module.esm.js`
+})
+
+console.log(`building plugin`)
+esbuild.buildSync({
+  entryPoints: [`remote-component-lite/builds/cdn.js`],
+  bundle: true,
+  platform: "browser",
+  outfile: `remote-component-lite/dist/cdn.js`
+})
+console.log(`building plugin`)
+esbuild.buildSync({
+  entryPoints: [`remote-component-lite/builds/cdn.js`],
+  bundle: true,
+  platform: "browser",
+  minify: true,
+  outfile: `remote-component-lite/dist/cdn.min.js`
+})
+console.log(`building plugin`)
+esbuild.buildSync({
+  entryPoints: [`remote-component-lite/builds/module.js`],
+  bundle: true,
+  mainFields: ["module", "main"],
+  platform: "neutral",
+  outfile: `remote-component-lite/dist/module.esm.js`
 })
